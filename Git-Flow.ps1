835121403e8a5f6113f88647e2df60de91f1a25b
Derek Flenniken <derek@enth.net>
2025-11-01 11:15:31 -0700

    FIX: can't use exit in function. it terminates the shell

--- Git-Flow.ps1
+++ Git-Flow.ps1
@@ -31,12 +31,9 @@ function Has-Remote {
     return $null -ne $REMOTE | Out-Null
 }
 
-function ExitOnError {
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-}
-
-function ExitIfNotVersionNumber {
+function VersionNumberIsValid {
     param(
+        [string]$Command,
         [string]$Name
     )
 
@@ -44,13 +41,29 @@ function ExitIfNotVersionNumber {
 
     if ($null -eq ($Version)) { 
         Write-Host "$Name is not a version number" -Fore Red
-        exit 1
+        Write-Host "Version Format: MAJOR.MINOR.PATCH (ex. 3.2.1)" -Fore Cyan
+        return $false
     }
 
     if(-1 -eq $Version.Build) { 
-        Write-Host "$Name is missing a revision number" -Fore Red
-        exit 1
+        Write-Host "$Name is missing a PATCH number" -Fore Red
+        Write-Host "Version Format: MAJOR.MINOR.PATCH (ex. 3.2.1)" -Fore Cyan
+        return $false
     }
+
+    if("release" -eq $Command -and 0 -ne $Version.Build){
+        Write-Host "Patch number should be 0 for a release" -Fore Red
+        Write-Host "Version Format: MAJOR.MINOR.PATCH (ex. 3.2.0)" -Fore Cyan
+        return $false
+    }
+
+    if("hotfix" -eq $Command -and 0 -eq $Version.Build){
+        Write-Host "Patch number should NOT be 0 for a hotfix" -Fore Red
+        Write-Host "Version Format: MAJOR.MINOR.PATCH (ex. 3.2.1)" -Fore Cyan
+        return $false
+    }
+
+    return $true
 }
 
 function Git-Flow {
@@ -67,10 +80,10 @@ function Git-Flow {
         "feature" {
             switch ($Action) {
                 "start" {
-                    Feature-Start $Name
+                    Feature-Start $Name | Out-Null
                 }
                 "finish" {
-                    Feature-Finish $Name
+                    Feature-Finish $Name | Out-Null
                 }
                 default {
                     Write-Host "Unknown Action:" $Action -Fore Red
@@ -81,10 +94,10 @@ function Git-Flow {
         "release" {
             switch ($Action) {
                 "start" {
-                    Release-Start $Name
+                    Release-Start $Name | Out-Null
                 }
                 "finish" {
-                    Release-Finish $Name
+                    Release-Finish $Name | Out-Null
                 }
                 default {
                     Write-Host "Unknown Action:" $Action -Fore Red
@@ -95,10 +108,10 @@ function Git-Flow {
         "hotfix" {
             switch ($Action) {
                 "start" {
-                    Hotfix-Start $Name
+                    Hotfix-Start $Name | Out-Null
                 }
                 "finish" {
-                    Hotfix-Finish $Name
+                    Hotfix-Finish $Name | Out-Null
                 }
                 default {
                     Write-Host "Unknown Action:" $Action -Fore Red
@@ -116,98 +129,99 @@ function Git-Flow {
 function Feature-Start {
     param([string]$Name)
 
-    git checkout $DEVELOP || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
-    git checkout -b feature/$Name || ExitOnError
+    git checkout $DEVELOP || { return }
+    Has-Remote || git pull --rebase || { return }
+    git checkout -b feature/$Name || { return }
 }
 
 function Feature-Finish {
     param([string]$Name)
 
-    git checkout feature/$Name || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout feature/$Name || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git checkout $DEVELOP || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout $DEVELOP || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git merge --no-ff --no-edit feature/$Name || ExitOnError
+    git merge --no-ff --no-edit feature/$Name || { return }
 
-    git branch -d feature/$Name || ExitOnError
+    git branch -d feature/$Name || { return }
 }
 
 function Release-Start {
     param([string]$Name)
 
-    ExitIfNotVersionNumber $Name
+    if(!(VersionNumberIsValid "release" $Name)) { return }
 
     # create release branch from develop
 
-    git checkout $DEVELOP || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
-    git checkout -b release/$Name || ExitOnError
+    git checkout $DEVELOP || { return }
+    Has-Remote || git pull --rebase || { return }
+    git checkout -b release/$Name || { return }
 }
 
 function Release-Finish {
     param([string]$Name)
 
-    ExitIfNotVersionNumber $Name
+    if(!(VersionNumberIsValid "release" $Name)) { return }
 
     # merge the release branch into main and tag it
-    git checkout release/$Name || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout release/$Name || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git checkout $MAIN || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout $MAIN || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git merge --no-ff --no-edit release/$Name || ExitOnError
-    git tag -a $Name || ExitOnError
+    git merge --no-ff --no-edit release/$Name || { return }
+    git tag -a $Name || { return }
 
     # merge the tag into develop
 
-    git checkout $DEVELOP || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
-    git merge --no-ff --no-edit $Name || ExitOnError
+    git checkout $DEVELOP || { return }
+    Has-Remote || git pull --rebase || { return }
+    git merge --no-ff --no-edit $Name || { return }
 
-    git branch -d release/$Name || ExitOnError
+    git branch -d release/$Name || { return }
 }
 
 function Hotfix-Start {
     param([string]$Name)
 
-    ExitIfNotVersionNumber $Name
+    if(!(VersionNumberIsValid "hotfix" $Name)) { return }
 
     # create hotfix branch from main
 
-    git checkout $MAIN || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
-    git checkout -b hotfix/$Name || ExitOnError
+    git checkout $MAIN || { return }
+    Has-Remote || git pull --rebase || { return }
+    git checkout -b hotfix/$Name || { return }
 }
 
 function Hotfix-Finish {
     param([string]$Name)
 
-    ExitIfNotVersionNumber $Name
+    if(!(VersionNumberIsValid "hotfix" $Name)) { return }
 
     # merge the hotfix branch into main and tag it
-    git checkout hotfix/$Name || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout hotfix/$Name || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git checkout $MAIN || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
+    git checkout $MAIN || { return }
+    Has-Remote || git pull --rebase || { return }
 
-    git merge --no-ff --no-edit hotfix/$Name || ExitOnError
-    git tag -a $Name || ExitOnError
+    git merge --no-ff --no-edit hotfix/$Name || { return }
+    git tag -a $Name || { return }
 
     # merge the tag into develop
 
-    git checkout $DEVELOP || ExitOnError
-    Has-Remote || git pull --rebase || ExitOnError
-    git merge --no-ff --no-edit $Name || ExitOnError
+    git checkout $DEVELOP || { return }
+    Has-Remote || git pull --rebase || { return }
+    git merge --no-ff --no-edit $Name || { return }
 
-    git branch -d hotfix/$Name || ExitOnError
+    git branch -d hotfix/$Name || { return }
 }
 
 If ($MyInvocation.InvocationName -ne ".")
 {
     Git-Flow $Command $Action $Name
+    exit $LASTEXITCODE
 }
